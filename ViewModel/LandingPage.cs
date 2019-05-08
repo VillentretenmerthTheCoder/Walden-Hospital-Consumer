37c515fc8425deeba6b72517cd523568c92c05df
Bartosz <DonMarchewa@gmail.com>
2019-05-08 10:28:37 +0200

    Landing Page added.

---
+++ ViewModel/LandingPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaldenHospitalConsumer.ViewModel
+{
+    class LandingPage
+    {
+
+        public void LogOut()
+        {
+
+        }
+
+        public void ShowListOfPatient()
+        {
+
+        }
+
+        public void RegistrationPage()
+        {
+
+        }
+
+    }
+}
