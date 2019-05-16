d1e5d43783a25c2e999a1a86ef96b3d85117639d
Bartosz <DonMarchewa@gmail.com>
2019-05-16 12:38:28 +0200

    PatientListViewModel update.

--- ViewModel/PatientListViewModel.cs
+++ ViewModel/PatientListViewModel.cs
@@ -5,83 +5,101 @@ using Windows.UI.Xaml.Controls;
 using System.Collections.ObjectModel;
 using WaldenHospitalConsumer.Model;
 using WaldenHospitalConsumer.View;
+using System.Linq;
 
 namespace WaldenHospitalConsumer.ViewModel
 {
-    class PatientListViewModel
+    class PatientListViewModel: NotificationClass
     {
-        public RelayCommand DoSearchByCpr;
-        public RelayCommand DoSearchByName;
-        public RelayCommand DoSearchBySurname;
-
         PatientCatalog PatientCatalog = new PatientCatalog();
         private ObservableCollection<Patient> _patients;
-        private ObservableCollection<Patient> _displayPatients;
-        private Patient _inputPatient;
-        
-        
-        public static Frame ContentFrame { get; set; }
+        private string _search;
 
+        
+
+        public ObservableCollection<Patient> Patients
+        {
+            get => _patients;
+            set
+            {
+                _patients = value;
+                OnPropertyChanged("Patients");
+            }
+        }
+
+        public string Search
+        {
+            get => _search;
+            set
+            {
+                if(Search != value)
+                {
+                    _search = value;
+                    UpdateSearch();
+                }
+            }
+        }
+
+        public RelayCommand SelectItemCommand { get; set; }
+
+        private void UpdateSearch()
+        {
+            Patients = Find(Search);
+        }
+
+        private ObservableCollection<Patient> Find(string Search)
+        {
+            ObservableCollection<Patient> _ReturnedList = new ObservableCollection<Patient>();
+            foreach(Patient _patient in PatientCatalog.Patients)
+            {
+                if(Convert.ToString(_patient.Cpr) == Search )
+                {
+                    _ReturnedList.Add(_patient);
+                }
+                else
+                if (Convert.ToString(_patient.User.Name) == Search)
+                {
+                    _ReturnedList.Add(_patient);
+                }
+                else
+                if(Convert.ToString(_patient.User.Surname) == Search)
+                {
+                    _ReturnedList.Add(_patient);
+                }
+            }
+            if (_ReturnedList == null)
+            {
+                return PatientCatalog.Patients;
+            }
+            else
+            {
+                return _ReturnedList;
+            }
+        }
+        
+        //private void SelectItem(object s)
+        //{
+
+        //}
+        
+        
+        
+        
         //Constructor
         public PatientListViewModel()
         {
+            //Navigation
             DoLogOut = new RelayCommand(LogOut);
             DoShowNewsView = new RelayCommand(ShowListOfPatient);
             DoShowRegistrationPage = new RelayCommand(ShowRegistrationPage);
-            DoSearchByCpr = new RelayCommand(SearchByCpr);
-            DoSearchByName = new RelayCommand(SearchByName);
-            DoSearchBySurname = new RelayCommand(SearchBySurname);
-            _inputPatient = new Patient();
-            _patients = new ObservableCollection<Patient>();
-            _displayPatients = new ObservableCollection<Patient>();
-            PatientInit();
+            //Searching
+
+
+          
         }
 
-        //Methods
-        private void PatientInit()
-        {
-            PatientCatalog.Patients = _patients;
-            _patients.Clear();
-        }
 
-        public void SearchByCpr(object s)
-        {
-            PatientInit();
-            _displayPatients.Clear();
-            foreach (var Patient in _patients)
-            {
-                if(Patient.Cpr == _inputPatient.Cpr)
-                {
-                    _displayPatients.Add(Patient);
-                }
-            }
-        }
-
-        public void SearchByName(object s)
-        {
-            PatientInit();
-            _displayPatients.Clear();
-            foreach(var Patient in _patients)
-            {
-                if(Patient.User.Name == _inputPatient.User.Name)
-                {
-                    _displayPatients.Add(Patient);
-                }
-            }
-        }
-
-        public void SearchBySurname (object s)
-        {
-            PatientInit();
-            _displayPatients.Clear();
-            foreach (var Patient in _patients)
-            {
-                if (Patient.User.Surname == _inputPatient.User.Surname)
-                {
-                    _displayPatients.Add(Patient);
-                }
-            }
-        }
+       
         
         //RelayCommands
         public RelayCommand DoLogOut { get; set; }
